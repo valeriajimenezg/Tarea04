56ae2ea45db0b60ce96b5179e9d268a086fbe969
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-22 17:14:10 -0600

    Tarea 4 Javascript

---
+++ Javascript/ejercicio13.js
@@ -0,0 +1,8 @@
+let jugador = prompt ("Introduzca el nombre de un jugador de fútbol: ")
+
+if (jugador == "Messi" || jugador == "Cristiano Ronaldo" || jugador == "Maradona"){
+    alert(jugador + " es uno de los mejores jugadores de fútbol del mundo.")
+}
+else {
+    alert( jugador + " no es uno de los mejores jugadores de fútbol del mundo.")
+}
