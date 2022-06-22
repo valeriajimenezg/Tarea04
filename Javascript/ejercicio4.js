a54526edda8999b1bc4b66a50df9d89f4fdd011f
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-22 15:41:36 -0600

    Tarea 4 Javascript - Jason Umana

--- Javascript/ejercicio4.js
+++ Javascript/ejercicio4.js
@@ -5,7 +5,7 @@ if ( saludo == "HOLA" || saludo == "Hola" || saludo == "hola"){
     alert ("Hola, buen día.")
 } else if ( saludo == "Hi" || saludo == "Hello" ){
     alert ("Hola, buen día.")
-} else if ( saludo == ola || saludo == Ola || saludo == OLA){
+} else if ( saludo == "ola" || saludo == "Ola" || saludo == "OLA"){
     alert ("Hola, buen día.")
 } else {
     alert ("Solo puedes decir: Hola")
