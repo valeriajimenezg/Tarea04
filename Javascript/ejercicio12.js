c97206f98f60bfb5941ecb6a096d76a82d328602
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-22 17:09:39 -0600

    Tarea 4 Javascript

---
+++ Javascript/ejercicio12.js
@@ -0,0 +1,8 @@
+let pais = prompt ("Introduzca el nombre de un país: ")
+
+if (pais == "Brasil" || pais == "Italia" || pais == "Francia" || pais == "Alemania" || pais == "Argentina" || pais == "Uruguay" || pais == "España" || pais == "Inglaterra"){
+    alert(pais + " ha ganado un mundial de fútbol.")
+}
+else {
+    alert( pais + " no ha ganado un mundial de fútbol.")
+}
