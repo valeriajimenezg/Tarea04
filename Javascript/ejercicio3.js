bcd7a710c858f73f14d61a84afb898580e7336cb
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-21 21:44:27 -0600

    Tarea 4 Javascript

---
+++ Javascript/ejercicio3.js
@@ -0,0 +1,98 @@
+let pais;
+pais = prompt ('Inserte el nombre de un país: ')
+
+if (pais == "Costa Rica"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "Argentina"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "Colombia"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "Nicaragua"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "Panama"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "Uruguay"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "Estados Unidos"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "Canada"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "Mexico"){
+    alert ( pais + " es un país de América.")
+}
+
+if (pais == "España"){
+    alert ( pais + " es un país de Europa.")
+}
+
+if (pais == "Italia"){
+    alert ( pais + " es un país de Europa.")
+}
+
+if (pais == "Francia"){
+    alert ( pais + " es un país de Europa.")
+}
+
+if (pais == "Rumania"){
+    alert ( pais + " es un país de Europa.")
+}
+
+if (pais == "Inglaterra"){
+    alert ( pais + " es un país de Europa.")
+}
+
+if (pais == "Alemania"){
+    alert ( pais + " es un país de Europa.")
+}
+
+if (pais == "Nigeria"){
+    alert ( pais + " es un país de África.")
+}
+
+if (pais == "Kenia"){
+    alert ( pais + " es un país de África.")
+}
+
+if (pais == "Ghana"){
+    alert ( pais + " es un país de África.")
+}
+
+if (pais == "Marruecos"){
+    alert ( pais + " es un país de África.")
+}
+
+if (pais == "Senegal"){
+    alert ( pais + " es un país de África.")
+}
+
+if (pais == "Nueva Zelanda"){
+    alert ( pais + " es un país de Oceanía.")
+}
+
+if (pais == "Australia"){
+    alert ( pais + " es un país de Oceanía.")
+}
+
+if (pais == "Fiji"){
+    alert ( pais + " es un país de Oceanía.")
+}
+
+if (pais == "Tonga"){
+    alert ( pais + " es un país de Oceanía.")
+}
