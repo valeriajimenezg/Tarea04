b604ef7c10095ca8f4c31b4e55cb6d10d65fa006
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-21 21:25:31 -0600

    Tarea 4 Javascript

---
+++ Javascript/ejercicio2.js
@@ -0,0 +1,16 @@
+let color;
+color = prompt("Inserte un color del semáforo: ");
+
+if(color == "Rojo"){
+    alert("Alto");
+}
+
+if(color == "Amarillo"){
+    alert("Precaución")
+}
+
+if(color == "Verde"){
+    alert("Siga")
+} else {
+    alert("Inserte una palabra válida.")
+}
