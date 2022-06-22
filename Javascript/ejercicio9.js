ba992ca4c1295a203b06aa2083220c3fdf96962d
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-22 16:57:35 -0600

    Tarea 4 Javascript

---
+++ Javascript/ejercicio9.js
@@ -0,0 +1,8 @@
+let comida = prompt ("Digite cual de los siguientes sabores desea: 1- Salado 2- Dulce")
+
+if ( comida == "1" ){
+    alert("Para la opcion " + comida + " hay pan de pizza y pan con jamon y queso")
+}
+if ( comida == "2" ){
+    alert("Para la opcion " + comida + " hay flauta de guayaba, cacho de dulce de leche y pañuelo de crema pastelera")
+}
