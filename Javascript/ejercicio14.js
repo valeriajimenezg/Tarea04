a55af538b90bc2da764d6f0660a01ea65e3e9d0b
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-21 21:16:09 -0600

    Tarea 4 Javascript

---
+++ Javascript/ejercicio14.js
@@ -0,0 +1,8 @@
+let numero;
+numero = prompt("Inserte un número: ");
+
+if ( numero % 2 == 0 ){
+    alert("El número " + numero + " es par.")
+} else if ( numero % 2 != 0 ){
+    alert("El número " + numero + " es impar.")
+}
