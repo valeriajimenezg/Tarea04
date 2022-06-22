ad66c7a66f63b2511de8f638e6c5f0cf6171798d
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-22 15:40:09 -0600

    Tarea 4 Javascript

---
+++ Javascript/ejercicio4.js
@@ -0,0 +1,12 @@
+let saludo;
+saludo = prompt("Di Hola!: ")
+
+if ( saludo == "HOLA" || saludo == "Hola" || saludo == "hola"){
+    alert ("Hola, buen día.")
+} else if ( saludo == "Hi" || saludo == "Hello" ){
+    alert ("Hola, buen día.")
+} else if ( saludo == ola || saludo == Ola || saludo == OLA){
+    alert ("Hola, buen día.")
+} else {
+    alert ("Solo puedes decir: Hola")
+}
