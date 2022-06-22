66e26162bb2b14de251e683bba59b0b5818da387
valeriajimenezg <106450213+valeriajimenezg@users.noreply.github.com>
2022-06-22 17:04:31 -0600

    Tarea 4 Javascript

---
+++ Javascript/ejercicio11.js
@@ -0,0 +1,8 @@
+let year = prompt ("Digite un año: ")
+
+if (year == "2010" || year == "2011" || year == "2012" || year == "2015" || year == "2019" || year == "2022"){
+    alert("En el año " + year + " Messi ganó un Balón de Oro.")
+}
+else {
+    alert("Messi no ganó el Balón de Oro en el año " + year)
+}
